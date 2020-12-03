fb203bcb65f94a1de90d596c4c02c0e1ceb6b6f5
andyrallis <andy.rallis@aon.com>
2020-12-03 18:51:53 +0800

    partial year

---
+++ ch4/ch4.hs
@@ -0,0 +1,24 @@
+ifEven myFunc x = if even x
+                  then myFunc x
+                  else x  
+
+
+lambdaEvenCube y = ifEven (\x -> x^3) y
+
+names = [("Ian", "Curtis"), ("Bernard","Sumner"), ("Peter", "Hook"), ("Stephen","Morris")]
+
+compareLastNames name1 name2 = if lastName1 > lastName2
+                               then GT
+                               else if lastName1 < lastName2 
+                                 then LT
+                                 else 
+                                  if firstName1 < firstName2
+                                  then GT
+                                  else if firstName1 > firstName2
+                                  then LT
+                                  else EQ      
+                                  
+  where lastName1 = snd name1
+        lastName2 = snd name2
+        firstName1 = fst name1
+        firstName2 = fst name2

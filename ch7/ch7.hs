9910557f962ee1a74348579be1406b15471d47fb
andy.rallis <andy.rallis@rallisfamily.com>
2020-12-07 23:28:46 +0800

    Finish Ch7

---
+++ ch7/ch7.hs
@@ -0,0 +1,17 @@
+myGcd a b = if remainder == 0
+            then b
+            else myGcd b remainder
+    where remainder = a `mod` b
+
+myEmpty [] = True
+myEmpty _list = False
+
+myTail1 (_x:xs) = xs
+
+--Q7.1
+myTail [] = []
+myTail (_x:xs) = xs
+
+--Q7.2
+myGcd2 a 0 = a
+myGcd2 a b = myGcd2 b (a `mod` b)

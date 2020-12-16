02bd8cb515a77c983b10ec266d46398e9696fdfa
andy.rallis <andy.rallis@rallisfamily.com>
2020-12-16 23:14:13 +0800

    Ch10 capstone finish

--- ch10/ch10.hs
+++ ch10/ch10.hs
@@ -20,3 +20,19 @@ hp (_, _, h) = h
 getName aRobot = aRobot name
 getAttack aRobot = aRobot attack
 getHP aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(_, a, h) -> robot (newName, a, h))
+setAttack aRobot newAttack = aRobot (\(n, _, h) -> robot (n, newAttack, h))
+setHP aRobot newHP = aRobot (\(n, a, _) -> robot (n, a, newHP))
+
+printRobot aRobot = aRobot
+  (\(name, attack, hp) ->
+    name ++ " attack: " ++ (show attack) ++ " hp: " ++ (show hp)
+  )
+
+damage aRobot attackDamage =
+  aRobot (\(n, a, h) -> robot (n, a, h - attackDamage))
+
+fight aRobot defender = damage defender attack
+  where attack = if getHP aRobot > 10 then getAttack aRobot else 0
+
